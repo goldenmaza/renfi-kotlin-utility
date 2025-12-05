aa560c0c571900fb553ff3e65d1277d741a13941
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-12-05 22:37:05 +0100

    [BRANCH] The BRANCH argument is introduced
    The BRANCH argument has now been introduced so we
    can also manage the associated application branches,
    just as we did with the FLOW arguments:
    
    1) The BRANCH_INDEX constant can be used to fetch
       the BRANCH_FLAG from the program arguments provided
       by the user.
    
    2) If the BRANCH_INDEX contains something else we throw
       an InvalidUseException to make sure that the application
       terminates accordingly and to alert the user. The 1 index
       is now locked to the BRANCH argument.
    
    3) The BRANCH task will now be logged, under the PROCESSING_TASK
       constant, which will be extended under the upcoming commits
       with more information.
    
    This commit also includes implementation and testing for:
    
    1) The Extraction feature, has been updated to hold the
       appropriate branch tasks, includes a new function which is
       used to hide away the number of upcoming 'when' statements
       that would otherwise clutter the code.
    
    Finally, there were some updates made to the previous test cases,
    as the program argument list required an update, but latter arguments
    are ignored as the negative test cases fail before they are even
    validated. Others are updated to include the associated branch.

--- src/main/java/org/hellstrand/renfikt/constant/ConstantExtractionUtil.kt
+++ src/main/java/org/hellstrand/renfikt/constant/ConstantExtractionUtil.kt
@@ -1,10 +1,26 @@
 package org.hellstrand.renfikt.constant
 
+import org.hellstrand.renfikt.constant.Constants.COMPARE_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.CONVERT_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.CROP_PROCESSING
 import org.hellstrand.renfikt.constant.Constants.DATA_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.DETECT_PROCESSING
 import org.hellstrand.renfikt.constant.Constants.FILE_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.JAVA_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.LABEL_COMPARE
+import org.hellstrand.renfikt.constant.Constants.LABEL_CONVERT
+import org.hellstrand.renfikt.constant.Constants.LABEL_CROP
 import org.hellstrand.renfikt.constant.Constants.LABEL_DATA_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.LABEL_DETECT
 import org.hellstrand.renfikt.constant.Constants.LABEL_FILE_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.LABEL_JAVA
+import org.hellstrand.renfikt.constant.Constants.LABEL_LIST
+import org.hellstrand.renfikt.constant.Constants.LABEL_ORIGIN
+import org.hellstrand.renfikt.constant.Constants.LABEL_SOURCE
 import org.hellstrand.renfikt.constant.Constants.LABEL_UNKNOWN_EXECUTION
+import org.hellstrand.renfikt.constant.Constants.LIST_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.ORIGIN_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.SOURCE_PROCESSING
 
 /**
  * @author (Mats Richard Hellstrand)
@@ -15,6 +31,20 @@ object ConstantExtractionUtil {
         return when (flowTask) {
             FILE_PROCESSING -> LABEL_FILE_PROCESSING
             DATA_PROCESSING -> LABEL_DATA_PROCESSING
+            else -> LABEL_UNKNOWN_EXECUTION
+        }
+    }
+
+    fun extractBranchTask(branchTask: String): String {
+        return when (branchTask) {
+            COMPARE_PROCESSING -> LABEL_COMPARE
+            CROP_PROCESSING -> LABEL_CROP
+            CONVERT_PROCESSING -> LABEL_CONVERT
+            DETECT_PROCESSING -> LABEL_DETECT
+            SOURCE_PROCESSING -> LABEL_SOURCE
+            JAVA_PROCESSING -> LABEL_JAVA
+            ORIGIN_PROCESSING -> LABEL_ORIGIN
+            LIST_PROCESSING -> LABEL_LIST
             else -> LABEL_UNKNOWN_EXECUTION
         }
     }
