34341f96d06ac28d2494676ee6eca77b2ad5903d
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-12-06 14:02:57 +0100

    [RESOURCE] The RESOURCE argument is introduced
    The RESOURCE argument has now been introduced so we
    can also manage the associated application resource,
    just as we did with the BRANCH arguments:
    
    1) The RESOURCE_INDEX constant can be used to fetch
       the RESOURCE_FLAG from the program arguments provided
       by the user.
    
    2) If the RESOURCE_INDEX contains something else we throw
       an InvalidUseException to make sure that the application
       terminates accordingly and to alert the user. The '2'
       index is now locked to the RESOURCE argument.
    
    3) The RESOURCE task will now be logged, under the
       PROCESSING_TASK constant, which will be extended under
       the upcoming commits with more information.
    
    This commit also includes implementation and testing for:
    
    1) The Extraction feature, has been updated to hold the
       appropriate resource tasks, includes a new function which
       is used to hide away the number of upcoming 'when'
       statements that would otherwise clutter the code.
    
    Finally, there were some updates made to the previous test
    cases, as the program argument list required an update, but
    latter arguments are ignored as the negative test cases fail
    before they are even validated. Others are updated to include
    the associated branch.

--- src/test/java/org/hellstrand/renfikt/RenfiUtility.kt
+++ src/test/java/org/hellstrand/renfikt/RenfiUtility.kt
@@ -3,11 +3,13 @@ package org.hellstrand.renfikt
 import org.hellstrand.renfikt.constant.Constants.COMPARE_PROCESSING
 import org.hellstrand.renfikt.constant.Constants.DATA_PROCESSING
 import org.hellstrand.renfikt.constant.Constants.FILE_PROCESSING
+import org.hellstrand.renfikt.constant.Constants.IMAGE_PROCESSING
 import org.hellstrand.renfikt.constant.Constants.JAVA_PROCESSING
 import org.hellstrand.renfikt.constant.Constants.MESSAGE_DISPLAY_HELP_GUIDE_EXCEPTION
 import org.hellstrand.renfikt.constant.ConstantsTest.ASSERT_ALLOWED_ARGUMENT_IS_USED_AND_LOGGED
 import org.hellstrand.renfikt.constant.ConstantsTest.ASSERT_FORMAT_MESSAGE_FUNCTION_TEST_INVALID_BRANCH_INDEX
 import org.hellstrand.renfikt.constant.ConstantsTest.ASSERT_FORMAT_MESSAGE_FUNCTION_TEST_INVALID_FLOW_INDEX
+import org.hellstrand.renfikt.constant.ConstantsTest.ASSERT_FORMAT_MESSAGE_FUNCTION_TEST_INVALID_RESOURCE_INDEX
 import org.hellstrand.renfikt.constant.ConstantsTest.HYPHEN_HELP_FLAG
 import org.hellstrand.renfikt.constant.ConstantsTest.INVALID_HYPHEN_FLAG
 import org.hellstrand.renfikt.exception.DisplayHelpGuideException
@@ -52,7 +54,7 @@ class RenfiUtilityTest {
     @DisplayName("Verifying that the main method fetches the FLOW argument and act accordingly when it is used incorrectly")
     fun mainMethodEntryTest_InvalidFlowFlagArgument_LogErrorAndThrowInvalidUseException() {
         // Prepare
-        val args = arrayOf(INVALID_HYPHEN_FLAG, INVALID_HYPHEN_FLAG)
+        val args = arrayOf(INVALID_HYPHEN_FLAG, INVALID_HYPHEN_FLAG, INVALID_HYPHEN_FLAG)
 
         // Execute
         val executable = Executable { RenfiUtility.main(args) }
@@ -66,7 +68,7 @@ class RenfiUtilityTest {
     @DisplayName("Verifying that the main method fetches the BRANCH argument and act accordingly when it is used incorrectly")
     fun mainMethodEntryTest_InvalidBranchFlagArgument_LogErrorAndThrowInvalidUseException() {
         // Prepare
-        val args = arrayOf(FILE_PROCESSING, INVALID_HYPHEN_FLAG)
+        val args = arrayOf(FILE_PROCESSING, INVALID_HYPHEN_FLAG, INVALID_HYPHEN_FLAG)
 
         // Execute
         val executable = Executable { RenfiUtility.main(args) }
@@ -77,10 +79,24 @@ class RenfiUtilityTest {
     }
 
     @Test
-    @DisplayName("Verifying that the main method fetches the FLOW argument (FileProcessing) and act accordingly when it is used correctly")
-    fun mainMethodEntryTest_ValidFlowAndBranchArguments_DisplayFileProcessingLogging() {
+    @DisplayName("Verifying that the main method fetches the RESOURCE argument and act accordingly when it is used incorrectly")
+    fun mainMethodEntryTest_InvalidResourceFlagArgument_LogErrorAndThrowInvalidUseException() {
         // Prepare
-        val args = arrayOf(FILE_PROCESSING, COMPARE_PROCESSING)
+        val args = arrayOf(FILE_PROCESSING, COMPARE_PROCESSING, INVALID_HYPHEN_FLAG)
+
+        // Execute
+        val executable = Executable { RenfiUtility.main(args) }
+
+        // Assert
+        val exception = assertThrows(InvalidUseException::class.java, executable)
+        assertEquals(ASSERT_FORMAT_MESSAGE_FUNCTION_TEST_INVALID_RESOURCE_INDEX, exception.message)
+    }
+
+    @Test
+    @DisplayName("Verifying that the main method fetches the FLOW argument (FileProcessing) and act accordingly when it is used correctly")
+    fun mainMethodEntryTest_ValidFlowAndBranchAndResourceArguments_DisplayFileProcessingLogging() {
+        // Prepare
+        val args = arrayOf(FILE_PROCESSING, COMPARE_PROCESSING, IMAGE_PROCESSING)
 
         // Execute
         RenfiUtility.main(args)
@@ -91,9 +107,9 @@ class RenfiUtilityTest {
 
     @Test
     @DisplayName("Verifying that the main method fetches the FLOW argument (DataProcessing) and act accordingly when it is used correctly")
-    fun mainMethodEntryTest_ValidFlowAndBranchArguments_DisplayDataProcessingLogging() {
+    fun mainMethodEntryTest_ValidFlowAndBranchAndResourceArguments_DisplayDataProcessingLogging() {
         // Prepare
-        val args = arrayOf(DATA_PROCESSING, JAVA_PROCESSING)
+        val args = arrayOf(DATA_PROCESSING, JAVA_PROCESSING, IMAGE_PROCESSING)
 
         // Execute
         RenfiUtility.main(args)
